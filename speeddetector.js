6e299189c0fc0047a0dca3317a8ecb0303bd7977
Skanner33 <mathewmageto@gmail.com>
2022-11-28 09:46:54 +0300

    Initial commit

---
+++ speeddetector.js
@@ -0,0 +1,15 @@
+function need(){
+    const speed = window.prompt("Enter speed", 70);
+
+    let i = 5;
+
+
+        if (speed < 70){
+            alert('Ok')
+        }else if(speed >= 71){
+            
+        }
+
+    
+}
+    
